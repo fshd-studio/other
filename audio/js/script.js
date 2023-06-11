ddff287ee980f38b9002e92c61fc77bcb9af9276
fastwalkergy <106460370+fastwalkergy@users.noreply.github.com>
2023-06-11 17:44:45 +0800

    c

--- audio/js/script.js
+++ audio/js/script.js
@@ -1,31 +1,14 @@
-const player = new Plyr("#my-audio", {
-    autoplay: true,
-    loop: {
-      active: true
-    },
-  });
+const titles = document.querySelectorAll('.accordion-title');
 
-  //控制台输出
-var styleTitle1 = `
-font-size: 20px;
-font-weight: 600;
-color: rgb(244,167,89);
-`
-var styleTitle2 = `
-font-size:150px;
-color: rgb(244,167,89);
-`
-var styleContent = `
-color: rgb(30,152,255);
-`
-var title1 = 'PSSD'
-var title2 = `
-PSSD                                               
-`
-var content = `
-版 本 号：1.2
-更新日期：2023-05-21
-
-`
-console.log(`%c${title1} %c${title2}
-%c${content}`, styleTitle1, styleTitle2, styleContent)
+titles.forEach(title => {
+    title.addEventListener('click', () => {
+        const content = title.nextElementSibling;
+        if (content.style.display === 'block') {
+            content.style.display = 'none';
+            title.classList.remove('is-open');
+        } else {
+            content.style.display = 'block';
+            title.classList.add('is-open');
+        }
+    });
+});
